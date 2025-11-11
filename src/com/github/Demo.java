d259ed72d40670801af395ae57a6c909742c700a
khaja <khaja@haldengroup.com>
2025-11-11 22:30:59 +0530

    Commit 3

--- src/com/github/Demo.java
+++ src/com/github/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Java Good Morning all here");
-		System.out.println("Welcome");
+		System.out.println("Welcome all");
 	}
 }
 	
